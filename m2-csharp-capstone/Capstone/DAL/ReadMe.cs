a9ae9aaf94a1655a7d9bae34247b6e34fccda59d
John Fulton <john@TechElevator.com>
2018-06-27 17:57:35 -0400

    initial commit

---
+++ m2-csharp-capstone/Capstone/DAL/ReadMe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.DAL
+{
+    class ReadMe
+    {
+        // In the DAL folder, create a .cs file for each each DAL
+        // and delete this .cs
+    }
+}
